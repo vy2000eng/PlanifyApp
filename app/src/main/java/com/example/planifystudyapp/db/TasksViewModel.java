411ab0e9f29770d1ca496174b992c7779e6e82a4
vy2000eng <vy2000eng@gmail.com>
2023-04-07 23:54:47 -0400

    added TasksViewModel

--- app/src/main/java/com/example/planifystudyapp/db/TasksViewModel.java
+++ app/src/main/java/com/example/planifystudyapp/db/TasksViewModel.java
@@ -1,4 +1,28 @@
 package com.example.planifystudyapp.db;
 
-public class TasksViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class TasksViewModel extends AndroidViewModel {
+
+    private LiveData<List<Tasks>> tasks;
+    public TasksViewModel(Application application){
+        super(application);
+        tasks = TasksDatabase.getDatabase(getApplication()).taskDao().getAll();
+    }
+    public void filterIsComplete(boolean isComplete){
+        if(isComplete)
+            tasks = TasksDatabase.getDatabase(getApplication()).taskDao().getUnCompleted(true);
+        else
+            tasks = TasksDatabase.getDatabase(getApplication()).taskDao().getAll();
+    }
+
+    public LiveData<List<Tasks>> getAllTasks() {
+        return tasks;
+    }
 }
