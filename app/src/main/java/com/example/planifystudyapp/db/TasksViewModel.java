b7c160e8f2014ac2632c58c56bf363b97ee7ff6f
vy2000eng <vy2000eng@gmail.com>
2023-04-08 12:14:23 -0400

    cardview

--- app/src/main/java/com/example/planifystudyapp/db/TasksViewModel.java
+++ app/src/main/java/com/example/planifystudyapp/db/TasksViewModel.java
@@ -1,4 +1,31 @@
 package com.example.planifystudyapp.db;
 
-public class TasksViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class TasksViewModel extends AndroidViewModel {
+    private LiveData<List<Tasks>> tasks;
+
+
+
+    public TasksViewModel(Application application){
+        super(application);
+        tasks = TasksDatabase.getDatabase(getApplication()).taskDao().getAll();
+    }
+    public void filterTasks(boolean isCompleted) {
+        if (isCompleted)
+            tasks = TasksDatabase.getDatabase(getApplication()).taskDao().getUnCompleted(true);
+        else
+            tasks = TasksDatabase.getDatabase(getApplication()).taskDao().getAll();
+    }
+
+
+    public LiveData<List<Tasks>> getAllTasks() {
+        return tasks;
+    }
+
 }
