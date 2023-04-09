d5af2b12e9c17b76d5ebb5ca656f7a6bccb2d054
vy2000eng <vy2000eng@gmail.com>
2023-04-08 22:34:23 -0400

    differentiating different tasks

--- app/src/main/java/com/example/planifystudyapp/ViewHolder/TaskListAdapter.java
+++ app/src/main/java/com/example/planifystudyapp/ViewHolder/TaskListAdapter.java
@@ -1,11 +1,14 @@
 package com.example.planifystudyapp.ViewHolder;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.planifystudyapp.R;
@@ -32,17 +35,25 @@ public class TaskListAdapter extends RecyclerView.Adapter <TaskListHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TaskListHolder holder, int position) {
+    public void onBindViewHolder( TaskListHolder holder, int position) {
         if(tasks != null){
             Tasks current = tasks.get(position);
             holder.task = current;
             holder.titleView.setText(current.title);
             holder.dueDate.setText(current.date);
             holder.description.setText(current.description);
+            int colorResId = current.priority == 0?R.color.colorLPDark
+                    :current.priority == 1?R.color.colorMPDark
+                    :current.priority ==2?R.color.colorHPDark:null;
+            int color = ContextCompat.getColor(holder.itemView.getContext(), colorResId);
+            holder.cardView.setCardBackgroundColor(color);
+
+
+
             if(current.isCompleted)
-                holder.isCompleted.setImageResource(R.drawable.assignment_icon);
-            else
                 holder.isCompleted.setImageResource(R.drawable.check_mark);
+            else
+                holder.isCompleted.setImageResource(R.drawable.assignment_icon);
         }else{
             holder.titleView.setText(R.string.init);
             holder.isCompleted.setImageResource(R.drawable.assignment_icon);
